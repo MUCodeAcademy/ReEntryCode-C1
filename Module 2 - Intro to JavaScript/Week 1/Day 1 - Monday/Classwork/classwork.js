81ba97cd4cdfa6dd3860841b37c287eb41ee7228
justinluce <luce@midlandu.com>
2024-02-12 03:05:36 -0600

    adds javascript day 1

---
+++ Module 2 - Intro to JavaScript/Week 1/Day 1 - Monday/Classwork/classwork.js
@@ -0,0 +1,13 @@
+// VARIABLE PRACTICE
+
+// -----------------------------------------------------------------
+//! Familiarize yourself with object traversal. (create an object and access its different properties)
+
+// -----------------------------------------------------------------
+//! Build an object with at least 3 layers deep of objects and practice logging to console.
+
+// -----------------------------------------------------------------
+//! Declare different variables of different types and compare/practice those.
+
+// -----------------------------------------------------------------
+//!! Demonstrate the difference between `==` and `===`

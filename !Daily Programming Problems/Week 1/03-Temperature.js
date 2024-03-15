25955a1d520685f20412ec556289e0bcdd47b2a5
justinluce <luce@midlandu.edu>
2024-03-15 04:16:37 -0500

    updates temperature problem

--- !Daily Programming Problems/Week 1/03-Temperature.js
+++ !Daily Programming Problems/Week 1/03-Temperature.js
@@ -5,7 +5,7 @@ Ask the user if they want to convert to Fahrenheit or Celsius
 
 Then, ask them for a number, and convert it based on which they chose
 
-To convert to Fahrenheit: input - 32 / 1.8
+To convert to Fahrenheit: (input - 32) / 1.8
 
 To convert to Celsius: input * 1.8 + 32
 */

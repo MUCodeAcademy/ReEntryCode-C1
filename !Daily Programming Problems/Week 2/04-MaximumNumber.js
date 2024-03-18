1e05496a45603409f16fc1ed6c6d8244d4b7b976
justinluce <luce@midlandu.edu>
2024-03-18 07:00:48 -0500

    adds day 1 week 2

---
+++ !Daily Programming Problems/Week 2/04-MaximumNumber.js
@@ -0,0 +1,17 @@
+/*
+Maximum Number Problem
+
+Find the greatest number in an array.
+
+Example:
+
+const numbers = [5, 7, 1, 8, 12, 3];
+
+Answer: 12
+*/
+
+function greatestNumber() {
+
+}
+
+greatestNumber();
